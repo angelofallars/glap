a6ed15f9b6b35784ac41d0124970bb0e13be46ad
Angelo Fallaria <ba.fallaria@gmail.com>
2022-06-11 02:35:15 +0800

    style: format with gofmt

--- grep.go
+++ grep.go
@@ -1,90 +1,89 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "bufio"
-    "log"
-    "strings"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
 type Options struct {
-    ignore_case bool
+	ignore_case bool
 }
 
 func main() {
-    arg_count := len(os.Args[1:])
+	arg_count := len(os.Args[1:])
 
-    if arg_count <= 0 {
-        fmt.Println("usage: grep [PATTERN]")
-        os.Exit(1)
-    }
+	if arg_count <= 0 {
+		fmt.Println("usage: grep [PATTERN]")
+		os.Exit(1)
+	}
 
-    options := Options{}
+	options := Options{}
 
-    var pattern string
-    pattern_arg_found := false
-    
-    for _, arg := range os.Args[1:] {
-        switch arg {
-            case "-i":
-            options.ignore_case = true
+	var pattern string
+	pattern_arg_found := false
 
-            default:
-            // Treat the first non-option argument as the filter pattern
-            pattern = arg
-            pattern_arg_found = true
-            break
-        }
-    }
+	for _, arg := range os.Args[1:] {
+		switch arg {
+		case "-i":
+			options.ignore_case = true
 
-    if !pattern_arg_found {
-        fmt.Println("usage: grep [PATTERN]")
-        os.Exit(1)
-    }
+		default:
+			// Treat the first non-option argument as the filter pattern
+			pattern = arg
+			pattern_arg_found = true
+			break
+		}
+	}
 
-    lines := read_input_lines()
+	if !pattern_arg_found {
+		fmt.Println("usage: grep [PATTERN]")
+		os.Exit(1)
+	}
 
-    // processed_lines is needed because the internal representation of
-    // things to filter won't always be the same as the original input,
-    // e.g. when ignoring letter case, where everything turns into uppercase.
-    var processed_lines []string
-    
-    // Clone the `lines` slice into the `processed_lines` slice
-    processed_lines = append(processed_lines, lines...)
-    
-    if options.ignore_case {
-        pattern = strings.ToUpper(pattern)
+	lines := read_input_lines()
 
-        for i := range processed_lines {
-            processed_lines[i] = strings.ToUpper(processed_lines[i])
-        }
-    }
+	// processed_lines is needed because the internal representation of
+	// things to filter won't always be the same as the original input,
+	// e.g. when ignoring letter case, where everything turns into uppercase.
+	var processed_lines []string
 
-    print_matching_lines(lines, processed_lines, pattern)
+	// Clone the `lines` slice into the `processed_lines` slice
+	processed_lines = append(processed_lines, lines...)
+
+	if options.ignore_case {
+		pattern = strings.ToUpper(pattern)
+
+		for i := range processed_lines {
+			processed_lines[i] = strings.ToUpper(processed_lines[i])
+		}
+	}
+
+	print_matching_lines(lines, processed_lines, pattern)
 }
 
 func print_matching_lines(orig_lines []string, lines []string, pattern string) {
-    for i, line := range lines {
+	for i, line := range lines {
 
-        if (strings.Contains(line, pattern)) {
-            fmt.Println(orig_lines[i])
-        }
-    }
+		if strings.Contains(line, pattern) {
+			fmt.Println(orig_lines[i])
+		}
+	}
 }
 
 func read_input_lines() []string {
-    input_scanner := bufio.NewScanner(os.Stdin)
-    input_lines := []string{}
+	input_scanner := bufio.NewScanner(os.Stdin)
+	input_lines := []string{}
 
 	for input_scanner.Scan() {
-        input_lines = append(input_lines, input_scanner.Text())
+		input_lines = append(input_lines, input_scanner.Text())
 	}
 
 	if error := input_scanner.Err(); error != nil {
-        log.Fatal(error)
+		log.Fatal(error)
 	}
 
-    return input_lines
+	return input_lines
 }
-
